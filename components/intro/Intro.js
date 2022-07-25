3b44ce4bae6007647576ff0c0721380c5d734cdb
Nadir <nadirkarimov76@gmail.com>
2022-07-25 05:04:33 -0400

    Create components

---
+++ components/intro/Intro.js
@@ -0,0 +1,10 @@
+import "./Intro.css"
+import React from 'react'
+
+function Intro() {
+  return (
+    <div className="intro"></div>
+  )
+}
+
+export default Intro
---
+++ components/sidebar/Sidebar.js
@@ -0,0 +1,10 @@
+import "./Sidebar.css"
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div className="sidebar"></div>
+  )
+}
+
+export default Sidebar
